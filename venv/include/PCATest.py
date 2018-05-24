2b430d9db324f5bfdbda8151f4770c1ef5b78869
WindAsMe <542636539@qq.com>
2018-05-25 01:04:45 +0800

    Face Detected; ;

---
+++ venv/include/PCATest.py
@@ -0,0 +1,53 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :18-5-18 下午8:06
+# File     :PCATest.py
+# Location:/Home/PycharmProjects/..
+
+# Principal Components Analysis(PCA)
+
+
+import numpy as np
+from sklearn import decomposition
+
+
+def do_pca():
+
+    # Define feature
+    # The last three feature is
+    # DEPEND on x1, x2
+    x1 = np.random.normal(size=250)
+    x2 = np.random.normal(size=250)
+    x3 = 2 * x1 + 3 * x2
+    x4 = 4 * x1 - x2
+    x5 = x3 + 2 * x4
+
+    # Create the collection of these statics
+    x = np.c_[x1, x3, x2, x5, x4]
+
+    # Create a PCA module
+    pca = decomposition.PCA()
+
+    # DO PCA
+    pca.fit(x)
+
+    # Print the VARIANCE
+    variances = pca.explained_variance_
+    print('Variances is decreasing order:\n', variances)
+
+    # Find out the useful dimensions
+    thresh_variance = 0.8
+    num_userful_dims = len(np.where(variances > thresh_variance)[0])
+    print('\nNumber of useful dimensions:', num_userful_dims)
+
+    # Only two dimension is useful
+    pca.n_components = num_userful_dims
+
+    x_new = pca.fit_transform(x)
+    print('\nShape before:', x.shape)
+    print('Shape after:', x_new.shape)
+
+
+if __name__ == '__main__':
+    do_pca()
---
+++ venv/include/faceIdTest.py
@@ -0,0 +1,56 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :18-5-17 下午3:09
+# File     :faceIdTest.py
+# Location:/Home/PycharmProjects/..
+
+# Face detected
+
+
+import cv2
+import numpy as np
+
+
+def face_detected():
+    # Initial the classifier
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+    # If classifier not found, raise error
+    if face_cascade.empty():
+        raise IOError("Not Found !")
+
+    # Open the video
+    cap = cv2.VideoCapture(0)
+
+    # Define the coefficient
+    scaling_factor = 0.5
+    while True:
+        # Collecting current frame
+        # DO PRETREATMENT
+        ret, frame = cap.read()
+        frame = cv2.resize(frame, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+        # Gray the pic
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Running the cascade
+        face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        # Draw the rectangle in face
+        for (x, y, w, h) in face_rects:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
+
+        # Show the pic
+        cv2.imshow('Face Detector', frame)
+
+        # Check 'Esc' is pressed
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+
+    # Release the resource
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    face_detected()
